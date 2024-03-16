36ecc791504f28331eb256dfb5cbbf3782ccf54c
Adetoke0987 <awesometokeh@gmail.com>
2024-03-16 22:01:40 +0100

    update

--- port.js
+++ port.js
@@ -1,3 +1,9 @@
+
+gsap.to(".bloop", {
+    scrollTrigger: ".bloop", // start animation when ".box" enters the viewport
+    x: 20,
+  });
+
 function toggleMenu() {
     var navbar = document.querySelector('.navbar');
     navbar.style.display = (navbar.style.display === 'block') ? 'none' : 'block';
