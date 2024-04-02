5032e6381ae0767e4648da5287de7fa2a5e87f3f
Adetoke Durojaiye <awesometokeh@gmail.com>
2024-04-02 16:05:02 +0100

    update

--- port.js
+++ port.js
@@ -1,10 +1,113 @@
+gsap.set("#tftoke", { perspective: 400 });
 
-gsap.to(".bloop", {
-    scrollTrigger: ".bloop", // start animation when ".box" enters the viewport
-    x: 20,
-  });
+gsap.from(".horizontal-rule, img,.soc, #p2, #stag, #my-name, #tftoke, #third, #part1, .hii> i", {
+  duration: 8,
+  opacity: 0,
+  scale: 0,
+  y: 10,
+  rotationX: 70,
+  transformOrigin: "0% 20% -30",
+  ease: "back",
+  stagger: 0.01
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function toggleMenu() {
     var navbar = document.querySelector('.navbar');
     navbar.style.display = (navbar.style.display === 'block') ? 'none' : 'block';
 }
+
+
+
+
+
